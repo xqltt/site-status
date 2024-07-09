a82fac273bda86b4af6e051ec5620b03b31b2c0a
xqltt <44520521+xqltt@users.noreply.github.com>
2024-07-09 17:02:33 +0800

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -47,7 +47,8 @@ const Header = observer(({ getSiteData }) => {
       messageApi.open({
         key: "updata",
         type: "info",
-        content: "The Mystery Team 主站相关服务宕机是因为国外检测节点被风控拦截,国内不受访问影响.",
+        content: "The Mystery Team 主站相关服务宕机是因为国外检测节点被风控拦截,国内不受访问影响.<br>目前国外检测节点对cdn服务器的访问可能会出现错误造成误报,国内不受影响.",
+        duration: 10,
       });
 
   return (
